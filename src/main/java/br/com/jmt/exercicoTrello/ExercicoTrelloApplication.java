30a495af2ed49cdaf95130f5131f9538508deb14
Davi Izui <davi.izui@gmail.com>
2025-09-02 11:07:31 -0300

    first commit

---
+++ src/main/java/br/com/jmt/exercicoTrello/ExercicoTrelloApplication.java
@@ -0,0 +1,13 @@
+package br.com.jmt.exercicoTrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicoTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicoTrelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jmt/exercicoTrello/ExercicoTrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jmt.exercicoTrello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicoTrelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
